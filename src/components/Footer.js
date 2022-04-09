66e114a388758fd0c5f64f49fa604272410f28c5
VISHAL <97962363+Vishal3474@users.noreply.github.com>
2022-04-09 11:10:54 +0530

    Update Footer.js
    link update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/Vishal3474"
+                href="https://www.instagram.com/its_vi.shal/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
